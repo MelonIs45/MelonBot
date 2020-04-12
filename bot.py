39f89958daf9cee265989c6af20f4c2b4edeeff8
MelonIs45 <45884578+MelonIs45@users.noreply.github.com>
2020-04-12 18:19:51 +0100

    added info command and improved overall embeds

--- bot.py
+++ bot.py
@@ -5,31 +5,41 @@ import os
 cwd = os.path.dirname(os.path.realpath(__file__))
 config = json.loads(open(cwd + "/config.json", "r").read())
 client = cmds.Bot(command_prefix = '$',  help_command=None)
-status = ['1', '12', '123', '1234', '12345']
+
 
 @client.event
 async def on_message(message):
-    #you can add a list of guilds to log
-    if message.guild.id == 657281286905266247:
+    masterChannelId = client.get_channel(int(config['log']['channelId']))
+    if message.guild.id == int(config['log']['logGuild']) and message.author != client.user:
+        if len(message.attachments) > 0:
+            attUrl = "".join(str(x) for x in message.attachments)
+            attUrl = attUrl.split(" ")[3]
+            attUrl = attUrl.lstrip("url='").rstrip("'>'")
+            logText = "{0} {1}".format(message.content, attUrl)
+        else:
+            attUrl = None
+            logText = message.content
         log = open("log.txt", "a")
-        logText = message.content
         logName = message.author.display_name
         currentDT = datetime.datetime.now()
         logTime = currentDT.strftime("%Y-%m-%d %H:%M:%S")
         logFull = "{0} | {1} | {2}".format(logTime, logName, logText)
-        log.write(logFull+"\n")
+        log.write(logFull + "\n")
+
+        embed = discord.Embed()
+        embed.set_author(name = message.guild.get_member(message.author.id), icon_url = message.author.avatar_url)
+        embed.description = f"{message.content} {attUrl}"
+        embed.set_footer(text = "channel: {0}".format(message.channel.name))
+        embed.timestamp = message.created_at
+        await masterChannelId.send(embed = embed)
         
     await client.process_commands(message)
 
 @client.command()
 async def help(message):
     user = message.author
-    reqMember = message.guild.get_member(user.id)
-    reqAvatar = reqMember.avatar_url_as(format='png')
 
-    embed = discord.Embed(
-        color = user.colour
-    )
+    embed = discord.Embed(color = user.colour)
     embed.title = 'MelonBot Help'
     embed.add_field(name = '$ping', value = 'Returns ping in ms.', inline = False)
     embed.add_field(name = '$avatar <user>', value = 'Shows the requested user their avatar, leave blank for your own.', inline = False)
@@ -39,7 +49,7 @@ async def help(message):
     embed.add_field(name = '$placeholder', value = 'placeholder', inline = False)
 
     embed.timestamp = datetime.datetime.utcnow()
-    embed.set_footer(text = f"Requested by {user.name}", icon_url = reqAvatar)
+    embed.set_footer(text = f"Requested by {message.guild.get_member(message.author.id)}", icon_url = message.guild.get_member(user.id).avatar_url_as(format='png'))
 
     await message.channel.send(embed = embed) 
 
@@ -70,8 +80,10 @@ async def someone(ctx, message):
 
 @client.command()
 async def avatar(ctx, mUser : discord.Member=None):
+    if ctx.author == client.user:
+        return
     if mUser == None:
-        mUser = ctx.message.guild.get_member(ctx.author.id)
+        mUser = ctx.message.guild.get_member(ctx.message.author.id)
         try:
             uAvatar = mUser.avatar_url_as(format='gif', size=256)
         except discord.errors.InvalidArgument:
@@ -81,32 +93,79 @@ async def avatar(ctx, mUser : discord.Member=None):
             uAvatar = mUser.avatar_url_as(format='gif', size=256)
         except discord.errors.InvalidArgument:
             uAvatar = mUser.avatar_url_as(format='png', size=256)
-        
+    
+    
+    #roles = mUser.roles
+    #roles = roles.split(", ")
+    #print(roles)
+    #attUrl = attUrl.split(" ")[3]
+    #attUrl = attUrl.lstrip("url='").rstrip("'>'")
     user = ctx.message.author
-    reqMember = ctx.message.guild.get_member(ctx.author.id)
-    reqAvatar = reqMember.avatar_url_as(format='png')
     embed = discord.Embed(color = user.colour)
     embed.set_author(name = mUser)
     embed.url = str(uAvatar)
     embed.title = "Avatar URL"
     embed.set_image(url = uAvatar)
     embed.timestamp = datetime.datetime.utcnow()
-    embed.set_footer(text = f"Requested by {user.name}", icon_url = reqAvatar)
+    embed.set_footer(text = f"Requested by {ctx.message.guild.get_member(ctx.message.author.id)}", icon_url = ctx.message.guild.get_member(user.id).avatar_url_as(format='png'))
 
     await ctx.send(embed = embed) 
 
 @client.command()
-async def info():
-    pass
+async def info(ctx, mUser : discord.Member=None):
+    if ctx.author == client.user:
+        return
+    if mUser == None:
+        mUser = ctx.message.guild.get_member(ctx.message.author.id)
+    user = ctx.message.author
+    print(ctx.message.author.roles)
+    roleTags = ""
+    for role in mUser.roles:
+        if role.name == "@everyone":
+            pass
+        else:
+            print(role.id)
+            roleTags += (f"<@&{role.id}> ")
+    print(roleTags)
+    
+    embed = discord.Embed(color = user.colour)
+    embed.set_author(name = ctx.message.guild.get_member(ctx.message.author.id))
+    embed.timestamp = datetime.datetime.utcnow()
+    embed.add_field(name = "**ID:**", value = ctx.message.author.id, inline = True)
+    embed.add_field(name = "**Nickname:**", value = mUser.nick, inline = True)
+    embed.add_field(name = "**Account Created:**", value = mUser.created_at.strftime("%A %d %B %Y at %X%p"), inline = False)
+    embed.add_field(name = "**Guild Join Date:**", value = mUser.joined_at.strftime("%A %d %B %Y at %X%p"), inline = False)
+    embed.add_field(name = "**Roles:**", value = roleTags, inline = False)
+    embed.set_footer(text = f"Requested by {ctx.message.guild.get_member(ctx.message.author.id)}", icon_url = ctx.message.guild.get_member(user.id).avatar_url_as(format='png'))
+
+    await ctx.send(embed = embed) 
 
 @client.command()
 async def ping(ctx):
+    if ctx.author == client.user:
+        return
+    user = ctx.message.author
+    embed = discord.Embed(color = user.color)
+    embed.timestamp = datetime.datetime.utcnow()
+    embed.set_author(name = ctx.message.guild.get_member(ctx.message.author.id))
     latency = (client.latency)*1000
-    await ctx.send('Pong! {0}ms'.format(round(latency, 1)))    
+    embed.description = 'Pong! {0}ms'.format(round(latency, 1))
+    embed.set_footer(text = f"Requested by {ctx.message.guild.get_member(ctx.message.author.id)}", icon_url = ctx.message.guild.get_member(user.id).avatar_url_as(format='png'))
+
+    await ctx.send(embed = embed)    
 
 @client.command()
 async def bertas(message):
-    await message.channel.send('Bertas is a nibba that has the big gay.')
+    if message.author == client.user:
+        return
+    user = message.author
+    embed = discord.Embed(color = user.color)
+    embed.timestamp = datetime.datetime.utcnow()
+    embed.set_author(name = message.guild.get_member(message.author.id))
+    embed.description = 'Bertas is a nibba that has the big gay.'
+    embed.set_footer(text = f"Requested by {message.guild.get_member(message.author.id)}", icon_url = message.guild.get_member(user.id).avatar_url_as(format='png'))
+
+    await message.channel.send(embed = embed)
 
 @client.event
 async def on_ready():
